b3ca24af6ec30af37e173f33c7676cf2682991fc
Ciffelia <mc.prince.0203@gmail.com>
2021-09-23 16:52:37 +0900

    Setup tsdoc linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   extends: ['standard-with-typescript', 'prettier'],
+  plugins: ['eslint-plugin-tsdoc'],
+  rules: {
+    'tsdoc/syntax': 'error'
+  },
   parserOptions: {
     project: './tsconfig.json'
   }
